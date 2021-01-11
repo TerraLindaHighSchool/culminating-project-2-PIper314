3859a76b5b129da0c53335ab2338401b7cb86a23
PIper314 <pe01790@srcs.org>
2021-01-10 22:30:24 -0800

    milestone 1.5 complete

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Transform tileObj;
+    private Vector3 nextTileSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextTileSpawn.x = 21;
+        StartCoroutine(spawnTile());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawnTile()
+    {
+        yield return new WaitForSeconds(1);
+        Instantiate(tileObj, nextTileSpawn, tileObj.rotation);
+        nextTileSpawn.x += 3;
+        StartCoroutine(spawnTile());
+    }
+
+
+}
